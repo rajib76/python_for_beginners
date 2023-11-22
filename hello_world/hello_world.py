1fc016f68b5b8127bfaefba82fb8ffd8707e74ca
rajib76 <rajib76@yahoo.com>
2023-11-22 10:34:14 -0800

    Initial commit

---
+++ hello_world/hello_world.py
@@ -0,0 +1,10 @@
+print(__name__)
+
+
+def print_hello_world():
+    print("hello world from package")
+
+
+if __name__ == "__main__":
+    print("I am from main")
+    print_hello_world()
---
+++ hello_world_01.py
@@ -0,0 +1,3 @@
+from hello_world.hello_world import print_hello_world
+
+print_hello_world()
